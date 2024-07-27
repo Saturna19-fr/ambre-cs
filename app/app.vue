91ffb0dfc61b091e48e4bee0a97c3decb4703fde
Thibault MOREZ <contact@saturna19.fr>
2024-07-27 22:15:26 +0000

    Init du repository

---
+++ app/app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Z</h1>
+    <NuxtLoadingIndicator />
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
+    <!-- <UNotifications /> -->
+  </div>
+</template>
